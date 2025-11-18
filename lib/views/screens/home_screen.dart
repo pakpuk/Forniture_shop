4cb56c3203f3c669f312055e3d18650b63f69034
Ghaith <ghayth.1900@gmail.com>
2025-11-19 00:54:49 +0100

    keep working on the main design

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:fourniture_shop/core/colors_manager.dart';
+import 'package:fourniture_shop/models/category_model.dart';
 import 'package:fourniture_shop/views/widgets/categories_box.dart';
 import 'package:fourniture_shop/views/widgets/custom_profile_bar.dart';
 import 'package:fourniture_shop/views/widgets/product_card_widget.dart';
@@ -17,6 +18,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    final CategoryModel category;
     return Scaffold(
       backgroundColor: ColorsManager.whiteColor,
       body: SafeArea(
@@ -30,16 +32,10 @@ class _HomeScreenState extends State<HomeScreen> {
               const Gap(25),
               SearchBarWidget(),
               const Gap(25),
-              ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: 5,
-                  itemBuilder: (context, index) {
-                    return const CategoriesBoxWidget(
-                      backgroundColor: ColorsManager.primayColor,
-                      textColor: ColorsManager.whiteColor,
-                    );
-                  }),
+              ListView.separated(
+                  itemBuilder: itemBuilder,
+                  separatorBuilder: separatorBuilder,
+                  itemCount: itemCount),
               const Gap(25),
               ListView.builder(
                   scrollDirection: Axis.horizontal,
